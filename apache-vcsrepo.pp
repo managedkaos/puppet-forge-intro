57c3c1ce4d0c0b572c645ccc6ffbe79352d92d54
Managed Kaos <michael@managedkaos.com>
2014-07-15 20:49:05 -0700

    Adding files.

---
+++ apache-vcsrepo.pp
@@ -0,0 +1,22 @@
+# define the docroot for use in multiple places
+$docroot = '/www'
+
+# install the git package
+package{ 'git':
+	ensure => installed,
+}
+
+# instantiate the apache class
+class { 'apache': }
+
+# configure the apache vhost
+apache::vhost { $::hostname:
+  docroot => $docroot,
+}
+
+# clone the repository
+vcsrepo { $docroot:
+    ensure   => present,
+    provider => git,
+    source   => "https://github.com/managedkaos/linuxla-puppet.git"
+}
---
+++ installation-steps.sh
@@ -0,0 +1,29 @@
+# Install Puppet
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+dpkg -i puppetlabs-release-precise.deb 
+apt-get update
+apt-get install -y puppet
+puppet --version
+
+# Check the Environment
+puppet module list
+which git
+puppet resource package git
+service apache2 status
+puppet resource service apache2
+curl -s -w "%{http_code}\n" -o /dev/null `hostname`.cloudapp.net
+
+# Install the Puppet Modules
+puppet module install puppetlabs-vcsrepo
+puppet module install puppetlabs-apache
+wget https://raw.githubusercontent.com/managedkaos/linuxla-puppet/master/linuxla-puppet.pp
+sudo puppet apply linuxla-puppet.pp
+
+# Check the Environment
+puppet module list
+which git
+puppet resource package git
+service apache2 status
+puppet resource service apache2
+curl -s -w "%{http_code}\n" -o /dev/null `hostname`.cloudapp.net
+
